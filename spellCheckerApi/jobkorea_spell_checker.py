1b9e9d879b60c0a486232875d16d1f839a903be2
aj4941@naver.com <aj4941@naver.com>
2024-03-27 11:27:56 +0900

    :sparkles: Jobkorea 맞춤법 API 작성

--- spellCheckerApi/jobkorea_spell_checker.py
+++ spellCheckerApi/jobkorea_spell_checker.py
@@ -27,59 +27,57 @@ def check(text):
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
     }
 
-    response = requests.post(base_url, data=payload, headers=headers, )
-    print(response.status_code)
-    print(response.headers)
-    print(json.dumps(response.text, ensure_ascii=False))
+    response = requests.post(base_url, data=payload, headers=headers)
 
     if response.status_code == 200:
-        data = parse_response(response.text)
-        # 여기서 `parse_response`는 응답 텍스트를 처리하고 필요한 데이터를 추출하는 함수입니다.
-        # 이 함수는 두 번째 코드에서 `parseJSON` 함수의 로직을 참고하여 구현해야 합니다.
-        # return data
+        data = parse_response(response.text, text)
+        return data
     else:
         return { 'result': False, 'message': 'Failed to get a valid response from the server.' }
 
-def parse_response(response_text):
+def parse_response(response_text, text):
     # Since `extract_data_from_html` already returns a Python object, we don't need to parse it again.
-    data = extract_data_from_html(response_text)
-
-    if not data:
-        return None  # or handle the error as appropriate
-
-    # 결과를 저장할 리스트를 초기화합니다.
+    data = json.loads(response_text)
+    print(data)
     results = []
 
     # `errInfo` 배열을 순회하면서 각 오류에 대한 정보를 추출합니다.
-    for error_info in data[0]['errInfo']:
+
+    current_idx = 0
+
+    for error_info in data['WordCandidateList']:
+        correct_method_value = calculate_correct_method(error_info['helpMessage'])
+
+        start_idx = calculate_idx(error_info['errorWord'], current_idx, text)
+        end_idx = start_idx + len(error_info['errorWord'])
         error_details = {
-            'help': error_info['help'],  # 오류를 해결하기 위한 도움말
-            'orgStr': error_info['orgStr'],  # 오류가 발생한 원래 문자열
-            'candWord': error_info['candWord'].split('|'),  # 교정 제안
-            'errorIdx': error_info['errorIdx'],  # 오류 인덱스
-            'correctMethod': error_info['correctMethod'],  # 교정 방법
-            'start': error_info['start'],  # 오류가 시작되는 위치
-            'end': error_info['end'],  # 오류가 끝나는 위치
+            'help': error_info['helpMessage'],  # 오류를 해결하기 위한 도움말
+            'orgStr': error_info['errorWord'],  # 오류가 발생한 원래 문자열
+            'candWord': remove_html(error_info['candidateWord']),  # 교정 제안
+            'errorIdx': start_idx,  # 오류 인덱스
+            'correctMethod': correct_method_value,  # 교정 방법
+            'start': start_idx,  # 오류가 시작되는 위치
+            'end': end_idx,  # 오류가 끝나는 위치
         }
         results.append(error_details)
+        current_idx = end_idx
 
     return results
+def remove_html(html_str):
+    # HTML 태그를 제거하고 결과물을 리스트로 변환합니다.
+    texts = re.findall(r'>[^<]+<', html_str)
+    # 각 텍스트에서 앞뒤의 '>'와 '<'를 제거합니다.
+    cleaned_texts = [text[1:-1] for text in texts]
+    return cleaned_texts
 
-def extract_data_from_html(html_content):
-    # `data` 변수에 할당된 JSON 데이터를 찾기 위한 정규 표현식
-    pattern = re.compile(r'data\s*=\s*(\[{.*?}\]);', re.DOTALL)
+def calculate_idx(error_str, current_idx, text):
+    start_idx = text.find(error_str, current_idx)
+    return start_idx
 
-    # 정규 표현식을 사용하여 HTML에서 JSON 문자열을 찾습니다.
-    match = pattern.search(html_content)
-    if match:
-        json_data = match.group(1)
-        try:
-            # JSON 문자열을 파싱하여 Python 객체로 변환합니다.
-            data = json.loads(json_data)
-            return data
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON: {e}")
-            return None
+def calculate_correct_method(str):
+    if '틀린 말' in str:
+        return 1
+    elif '붙' in str or '띄' in str:
+        return 2
     else:
-        print("No data found")
-        return None
+        return 1
--- spellCheckerApi/views.py
+++ spellCheckerApi/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 def busanSpellChecker(request):
     # request.body를 읽어 문자열로 변환합니다.
     body_str = request.body.decode('utf-8')
-    result = busan_spell_checker.check(u'서울서울에서 서울에서살럅니댜 알갰습니까?')
+    result = busan_spell_checker.check(body_str)
 
     # 처리 결과를 JSON으로 반환합니다.
     return HttpResponse(
@@ -19,7 +19,7 @@ def busanSpellChecker(request):
 @csrf_exempt
 def jobKoreaSpellChecker(request):
     body_str = request.body.decode('utf-8')
-    result = jobkorea_spell_checker.check(u'서울서울에서 서울에서살럅니댜 알갰습니까?')
+    result = jobkorea_spell_checker.check(body_str)
 
     return HttpResponse(
         json.dumps(result, ensure_ascii=False),
