8d252cd6f3be239906c188cd7ca440184783d8fd
un-known0 <nam0221@naver.com>
2024-09-11 01:00:07 +0900

    [hotfix] escape sequence error hotfix

--- spell_checker/busan_spell_checker.py
+++ spell_checker/busan_spell_checker.py
@@ -1,6 +1,8 @@
 import re
 import requests
 import json
+from spell_checker.all_spell_checker import adjust_indices_for_escape_characters
+
 
 # 수정된 부분: `base_url`을 부산대학교 맞춤법 검사기 URL로 설정
 base_url = 'http://speller.cs.pusan.ac.kr/results'
@@ -28,37 +30,42 @@ def check(text):
     response = requests.post(base_url, data=payload, headers=headers)
 
     if response.status_code == 200:
-        data = parse_response(response.text)
+        data = parse_response(response.text, text)
         # 여기서 `parse_response`는 응답 텍스트를 처리하고 필요한 데이터를 추출하는 함수입니다.
         # 이 함수는 두 번째 코드에서 `parseJSON` 함수의 로직을 참고하여 구현해야 합니다.
         return data
     else:
         return {'result': False, 'message': 'Failed to get a valid response from the server.'}
 
-def parse_response(response_text):
-    # Since `extract_data_from_html` already returns a Python object, we don't need to parse it again.
+def parse_response(response_text, original_text):
     data = extract_data_from_html(response_text)
-    
-    if not data:
-        return None  # or handle the error as appropriate
 
-    # 결과를 저장할 리스트를 초기화합니다.
+    if not data:
+        return None
+
     results = []
-    
-    # `errInfo` 배열을 순회하면서 각 오류에 대한 정보를 추출합니다.
+
     for error_info in data[0]['errInfo']:
+        # 오류의 원래 start, end 인덱스
+        start = error_info['start']
+        end = error_info['end']
+
+        # 이스케이프 문자로 인한 인덱스 보정
+        adjusted_start, adjusted_end = adjust_indices_for_escape_characters(original_text, start, end)
+
         error_details = {
             'help': error_info['help'],  # 오류를 해결하기 위한 도움말
             'orgStr': error_info['orgStr'],  # 오류가 발생한 원래 문자열
             'candWord': error_info['candWord'].split('|'),  # 교정 제안
             'errorIdx': error_info['errorIdx'],  # 오류 인덱스
             'correctMethod': error_info['correctMethod'],  # 교정 방법
-            'start': error_info['start'],  # 오류가 시작되는 위치
-            'end': error_info['end'],  # 오류가 끝나는 위치
+            'start': adjusted_start,  # 보정된 start 인덱스
+            'end': adjusted_end,  # 보정된 end 인덱스
         }
         results.append(error_details)
-    
+
     return results
+
 
 def extract_data_from_html(html_content):
     # `data` 변수에 할당된 JSON 데이터를 찾기 위한 정규 표현식
